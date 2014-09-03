6a11a9ab843729414e7b69c9368864b61aa67f8d
Guwanjith <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-03 23:03:10 +0530

    new

--- Challenge: Seconds Alive/main.m
+++ Challenge: Seconds Alive/main.m
@@ -13,8 +13,23 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSDateComponents *comps = [[NSDateComponents alloc] init];
+        [comps setYear:1991];
+        [comps setMonth:11];
+        [comps setDay:15];
+        [comps setHour:5];
+        [comps setMinute:30];
+        [comps setSecond:0];
+        
+        NSCalendar *g = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+        
+        //Dates to Compare
+        NSDate *myBirthday = [g dateFromComponents:comps];
+        NSDate *now = [NSDate date];
+        
+        double secondsBetween = [now timeIntervalSinceDate:myBirthday];
+        
+        NSLog(@"There are %f seconds between your birthday and today.\n", secondsBetween);
         
     }
     return 0;
